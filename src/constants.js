b01b576c35aa4e08542529da67711a0ba95ae218
harshavvardhan01 <65484883+harshavvardhan01@users.noreply.github.com>
2025-01-16 23:57:58 +0530

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,39 @@
+
+export const services = [
+    {
+        title: "Technology Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Software Engineer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Cricketer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Harsha';
+
+export const experiences = [
+    {
+        'company': 'Tata Consultancy Services',
+        'role': 'Frontend Developer',
+        'duration': 'November 2021 - Present',
+        'logo': 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Tata_Consultancy_Services_Logo.svg/1200px-Tata_Consultancy_Services_Logo.svg.png',
+        'points': [
+            'I have been part of an agile development team that develops and maintains Chrome extension “Sidekick” product with regular release cycles.',
+            'Being joined as a fresher soon I started undertaking major work items that added value to the clients.',
+            'Sidekick Chrome Extension, an application which serves as an on-the-go assistant that delivers context-sensitive information directly within Google Chrome, improving efficiency in daily work tasks.',
+            'Sidekick Plugin (Browser Extension): an react and Rest APIbased application. This is the browser extension which takes care of injecting UI extensions (page extensions) within any Sales Application (Harmony, Totango, Outreach).'
+        ],
+        'url': 'https://www.tcs.com/',
+    }
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = process.env.REACT_APP_EMAIL_JS_SERVICE_ID;
+export const EMAIL_JS_TEMPLATE_ID = process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID;
+export const EMAIL_JS_PUBLIC_KEY = process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY;
